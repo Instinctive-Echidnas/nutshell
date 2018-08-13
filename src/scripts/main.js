192b9939b71c9ff89d9e128b3e4c7d5027efdc02
Figamus <fig74656@yahoo.com>
2018-08-13 15:56:35 -0500

    Getting the login-dashboard calls to operate

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 console.log("Hello main.js");
+
+tempObject  = {
+    userName: false
+}
+
+sessionStorage.setItem("session", JSON.stringify(tempObject));
+
+let login = JSON.parse(sessionStorage.getItem("session"));
+
+if (login.userName) {
+    console.log("Dashboard");
+} else {
+    console.log("Login");
+}
