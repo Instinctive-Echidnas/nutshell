295864bdde27383dd7a058c455ab2aef7b7b9da0
nicholascblack4@gmail.com <nicholascblack4@gmail.com>
2018-08-14 12:50:27 -0500

    adding a commit since welcome.js has been updated to reflect a button for registering a new account that displays a registration form :neckbeard:

--- src/scripts/login/welcome.js
+++ src/scripts/login/welcome.js
@@ -3,13 +3,28 @@
 console.log("welcome.js connected");
 
 const welcomeMsg = "<h1>Welcome to Nutshell! Please register/login to continue<br/></h1>";
+const btn = document.createElement("button");
+const textNode = document.createTextNode("register new account");
+btn.appendChild(textNode);
+const documentBody = document.querySelector("BODY");
+
+const formParagraph = document.createElement("formParagraph");
 
 
 
 const welcome = function() {
     const welcomeParagraph = document.createElement("p");
     welcomeParagraph.innerHTML += welcomeMsg;
-    document.querySelector("BODY").appendChild(welcomeParagraph);
+    documentBody.appendChild(welcomeParagraph);
+    documentBody.appendChild(btn);
 }
+
+// i neeedd an event listener for button to display a form
+btn.addEventListener("click", function displayForm() {
+    console.log("button clicked so display form to DOM");
+    // display form
+    formParagraph.innerHTML = "<h2>FORM HERE</h2>";
+    documentBody.appendChild(formParagraph);
+});
 
 module.exports = welcome;
