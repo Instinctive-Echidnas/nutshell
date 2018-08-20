150469596dd1d5a8247ec68bdd4dc5beba20229b
Figamus <fig74656@yahoo.com>
2018-08-19 23:53:23 -0500

    Commiting to check pull request

--- src/scripts/chat/chatForm.js
+++ src/scripts/chat/chatForm.js
@@ -8,9 +8,9 @@ makeChat = (response) => {
         .then(username => {
             let messageBox = $("<div></div>").attr("id", `messageBox--${index}`).attr("class", "messages");
             let messageArea = $("<div></div>").attr("class", "chatMessage").text(`${messageObject.message}`);
-            let userBox = $("<div></div>").attr("class", "userName").text(username);
-            let editButton = $("<button>Edit</button>").attr("id", `editchatButton--${messageObject.id}`);
-            let deleteButton = $("<button>Delete</button>").attr("id", `deletechatButton--${messageObject.id}`);
+            let userBox = $("<div></div>").attr("class", "chatUserName").text(username);
+            let editButton = $("<div></div>").text("Edit").attr("id", `editchatButton--${messageObject.id}`).attr("class", `editChatButton`);
+            let deleteButton = $("<div></div>").text("Delete").attr("id", `deletechatButton--${messageObject.id}`).attr("class", `deleteChatButton`);
             $(messageBox).append(userBox, messageArea, editButton, deleteButton);
             $("#chatHistory").append(messageBox);
             $("#chatHistory").scrollTop($("#chatHistory")[0].scrollHeight) //THIS MAKES THE CHAT WINDOW STAY SCROLLED TO THE MOST CURRENT POST})
